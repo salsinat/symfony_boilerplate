d88c1cef5098993370e9eb637df433866d3d6cd3
stanguilper <stanguilper@gmail.com>
2025-09-19 15:04:40 +0000

    Ajout d'un css

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,9 +8,9 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'home')]
+    #[Route('/', name: 'home')]
     public function index(): Response
     {
-        return $this->render('base.html.twig');
+        return $this->render('home.html.twig');
     }
 }
